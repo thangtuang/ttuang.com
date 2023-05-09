1f22695ca6bce148ebe7709dd20f1f496caef9df
thangtuang <jhntuang1@gmail.com>
2023-05-09 18:32:42 -0400

    added to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Blog from "./Pages/Blog";
+import Projects from "./Pages/Projects";
+import Home from "./Pages/Home";
+import Services from "./Pages/Services";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Services" element={<Services />} />
+      </Routes>
     </div>
   );
 }
